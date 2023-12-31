6781901a26bb75ff42e5b22ce703d3ec40fe7511
Arkadiusz Daugielewicz <akson_control@o2.pl>
2023-12-31 19:45:59 +0100

    fix: eslint-disable-next line Movie.js:17

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,7 @@ export const App = () => {
   const navigate = useNavigate();
   useEffect(() => {
     navigate("/");
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <Routes>
